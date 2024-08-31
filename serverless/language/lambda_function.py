4a735e41e74515b6bde0b17f7067ebd75d3f76d7
muhammad nur rahman <138513675+sarrahman-me@users.noreply.github.com>
2024-08-31 21:29:47 +0700

    :recycle: memperbaiki code handler

--- serverless/language/lambda_function.py
+++ serverless/language/lambda_function.py
@@ -5,20 +5,24 @@ from handlers import add_language, get_language_by_id, get_all_language
 def lambda_handler(event, context):
     http_method = event.get("httpMethod")
     resource_path = event.get("resource")
-    path_parameters = event.get("pathParameters", {})
 
     if http_method == "POST" and resource_path == "/language":
-        return add_language(event)
+        body = json.loads(event["body"])
+        language_name = body["name"]
+        url_image = body["url_image"]
+
+        return add_language(language_name, url_image)
+
     elif http_method == "GET" and resource_path == "/language":
         return get_all_language(event)
+
     elif http_method == "GET" and resource_path.startswith("/language/"):
-        # Extract the ID from the path
-        language_id = resource_path.split("/")[-1]
-        event["pathParameters"] = {"id": language_id}
-        return get_language_by_id(event)
+        language_id = event["pathParameters"]["id"]
+        return get_language_by_id(language_id)
+
     else:
         return {
-            "statusCode": 405,  # Method Not Allowed
+            "statusCode": 405,
             "body": json.dumps({"message": "Method Not Allowed"}),
             "headers": {"Content-Type": "application/json"},
         }
--- serverless/language/test.py
+++ serverless/language/test.py
@@ -3,40 +3,29 @@ import json
 
 
 def test_add_language():
-    # Add a new language
-    event = {"body": '{"name": "C"}'}
-    response = add_language(event)
+    response = add_language(name="C", url_image="Image dummy")
 
     assert response["statusCode"] == 200
     assert json.loads(response["body"])["message"] == "Language added successfully"
 
 
 def test_add_language_conflict():
-    # Add the same language again to test conflict
-    event = {"body": '{"name": "C"}'}
-    response = add_language(event)
+    response = add_language(name="C", url_image="Image dummy")
 
     assert response["statusCode"] == 409
     assert json.loads(response["body"])["message"] == "Language 'C' already exists."
 
 
 def test_get_all_language():
-    # Ensure that there is at least one language to retrieve
-    event = {}
-    response = get_all_language(event)
+    response = get_all_language()
 
     assert response["statusCode"] == 200
     data = json.loads(response["body"])["data"]
-    assert isinstance(data, list)  # Assuming data should be a list
+    assert isinstance(data, list)
 
 
 def test_get_language_by_id():
-    # Ensure that there is a language with a known ID
-    # Insert a sample language with ID 1 for testing
-    # In a real test, you would mock the database or use a test database
-    # This is a simple example assuming ID 1 exists
-    event = {"pathParameters": {"id": 1}}
-    response = get_language_by_id(event)
+    response = get_language_by_id(18)
 
     assert response["statusCode"] == 200
     assert json.loads(response["body"])["message"] == "Language retrieved successfully"
